966ad08a522bab11f52f11cf1a1755ae03e6a35f
Sasidharsai-2006 <sasidharsai26@gmail.com>
2026-01-05 21:25:29 +0530

    Added GFG solution: Missing Number

---
+++ gfg-solutions/arrays/MissingNumber.java
@@ -0,0 +1,12 @@
+
+class MissingNumber {
+    int missingNumber(int arr[]) {
+       int n=arr.length+1;
+       long totalsum=(long)n*(n+1)/2;
+       long sum=0;
+       for(int i=0;i<arr.length;i++){
+           sum=sum+arr[i];
+       }
+       return (int)(totalsum-sum);
+    }
+}
