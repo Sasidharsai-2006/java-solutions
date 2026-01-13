69ee1092c325c1cd022785c4d78d1275a94fb7d9
Sasidharsai-2006 <sasidharsai26@gmail.com>
2026-01-13 11:22:57 +0530

    RotateArray

---
+++ gfg-solutions/arrays/RotateArray.java
@@ -0,0 +1,19 @@
+class RotateArray {
+    static void rotateArr(int arr[], int d) {
+        int n=arr.length;
+        if(n==0)return ;
+        d=d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+    public static void reverse(int arr[],int low,int high){
+        while(low<=high){
+            int temp=arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+        }
+    }
+}
