38942f4f1dd1396d3b34c0e8169dcc700b96c183
Sasidharsai-2006 <sasidharsai26@gmail.com>
2026-02-16 21:39:23 +0530

    removeDuplicates

---
+++ gfg-solutions/strings/StringDuplicatesRemoval.java
@@ -0,0 +1,14 @@
+public class StringDuplicatesRemoval {
+  String removeDuplicates(String s) {
+        LinkedHashSet<Character> ss=new LinkedHashSet<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            ss.add(ch);
+        }
+        StringBuilder ans=new StringBuilder();
+        for(char ch :ss){
+            ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
