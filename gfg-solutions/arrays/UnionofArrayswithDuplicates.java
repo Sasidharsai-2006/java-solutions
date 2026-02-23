17653b5c9717d39a53714146b5cd0fd21b23c5cc
Sasidharsai-2006 <sasidharsai26@gmail.com>
2026-02-23 08:14:07 +0530

    findUnion

---
+++ gfg-solutions/arrays/UnionofArrayswithDuplicates.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+public class UnionofArrayswithDuplicates {
+  public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        // code here
+        TreeSet<Integer> ss=new TreeSet<>();
+        for(int i=0;i<a.length;i++){
+            ss.add(a[i]);
+        }
+        for(int i=0;i<b.length;i++){
+            ss.add(b[i]);
+        }
+        ArrayList<Integer> ans=new ArrayList<>(ss);
+        return ans;
+    }
+}
