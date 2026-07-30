2cb807a9a6de86f863b852a5d36431ec260f170a
Sasidharsai-2006 <sasidharsai26@gmail.com>
2026-07-30 18:30:29 +0530

    wordcnt

---
+++ gfg-solutions/strings/MinimumNumberofPushestoTypeWordI.java
@@ -0,0 +1,10 @@
+class MinimumNumberofPushestoTypeWordI{
+  public int minimumPushes(String word) {
+        int gm=0;
+        for(int i=0;i<word.length();i++)
+        {
+            gm+=i/8+1;
+        }
+        return gm;
+    }
+}
